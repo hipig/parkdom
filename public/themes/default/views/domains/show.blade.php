cbfe481d738b1a5af26a081ed0e2583457566bf6
hipig <205270006@qq.com>
2021-11-26 09:43:57 +0800

    修改 .gitignore

--- public/themes/default/views/domains/show.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('title', $domain->seo_title ?? "{$domain->domain} is on sale")
-
-@section('content')
-    <main class="min-h-screen flex flex-col text-white bg-gray-900">
-        <header></header>
-        <div class="text-4xl font-bold">{{ $domain->domain }}</div>
-    </main>
-@endsection
